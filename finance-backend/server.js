416d0564f3dbd1bc0b3f4183555dbcec5a74f070
Prajwal <prajwalthakur2004@gmail.com>
2026-09-12 16:14:54 +0530

    prepare backend for deployment

--- finance-backend/server.js
+++ finance-backend/server.js
@@ -5,6 +5,6 @@ const app = require('./src/app');
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
